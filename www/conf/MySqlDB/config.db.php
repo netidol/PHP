f9e9a68a5870fa390d89194afbf07b66f73eb91b
netidol <netidol@163.com>
2016-06-30 23:06:47 +0800

    .*

--- www/conf/MySqlDB/config.db.php
+++ www/conf/MySqlDB/config.db.php
@@ -1,9 +1,9 @@
 <?php 
 //使用mycat中间件的配置，读写分离
-    $db_config["hostname"] = "10.10.11.174:3306"; //数据库ip和端口，默认3306
-    $db_config["username"] = "ueEbNyrKpv3cwluM"; //用户名
-    $db_config["password"] = "peuFVaywBXKLgkEjI"; //密码
-    $db_config["database"] = "JlRi2kwOsVALd4jn"; //数据库schema
+    $db_config["hostname"] = "94itao.eicp.net:8066"; //数据库ip和端口，默认3306
+    $db_config["username"] = "root"; //用户名
+    $db_config["password"] = "itdev@123"; //密码
+    $db_config["database"] = "track"; //数据库schema
     $db_config["charset"] = "utf8";//数据编码utf-8
     $db_config["pconnect"] = 0;//开启持续连接
     $db_config["log"] = 0;//是否开启日志，0,1
