ae7886854bf2954fbc7d12d66b51d2a158acae87
netidol <netidol@163.com>
2016-07-04 14:44:56 +0800

    Signed-off-by: netidol <netidol@163.com>

---
+++ www/MySqlDB/config.db.php
@@ -0,0 +1,10 @@
+<?php 
+    $db_config["hostname"] = "192.168.188.104"; //数据库ip和端口，默认3306
+    $db_config["username"] = "root"; //用户名
+    $db_config["password"] = "itdev@123"; //密码
+    $db_config["database"] = "app_liangguanliapi"; //数据库schema
+    $db_config["charset"] = "utf8";//数据编码utf-8
+    $db_config["pconnect"] = 0;//开启持续连接
+    $db_config["log"] = 0;//是否开启日志，0,1
+    $db_config["logfilepath"] = './';//日志目录
+?>
--- www/index.php
+++ www/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require('DB.php');
+require('mysqlDb.php');
 
 $db = new DB();
 
